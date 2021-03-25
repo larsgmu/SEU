363aa951a91a992add3dad5bc446248ee92e5a77
Knut Vågnes Eriksen <knutve@stud.ntnu.no>
2021-03-25 11:05:12 +0100

    project added

---
+++ third_project/Core/Src/main.c
@@ -0,0 +1,383 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include <string.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_ADC_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+	uint32_t adc_value;
+	char msg[10];
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_ADC_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+
+//	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_SET);
+//	  HAL_Delay (100);   /* Insert delay 1000 ms */
+//	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_RESET);
+//	  HAL_Delay (300);   /* Insert delay 1000 ms */
+//
+//	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
+//	  HAL_Delay (100);   /* Insert delay 1000 ms */
+//	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+//	  HAL_Delay (300);   /* Insert delay 1000 ms */
+//
+//	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);
+//	  HAL_Delay (100);   /* Insert delay 1000 ms */
+//	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_RESET);
+//	  HAL_Delay (300);   /* Insert delay 1000 ms */
+
+	  GPIO_PinState button_1 = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_9);
+	  GPIO_PinState button_2 = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_7);
+
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, button_1);
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, button_1);
+	  //HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, button_2);
+
+	  HAL_ADC_Start(&hadc);
+	  HAL_ADC_PollForConversion(&hadc, HAL_MAX_DELAY);
+	  adc_value = HAL_ADC_GetValue(&hadc);
+	  float voltage = (float)adc_value / 4095.0 * 3.3;
+
+	  if (voltage > 1.5){
+		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);
+	  }
+	  else {
+		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_RESET);
+	  }
+
+	  sprintf(msg, "%hu\r\n", adc_value);
+	  HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+
+	  HAL_Delay(1000);
+
+
+
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = 0;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_5;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
+  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief ADC Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC_Init(void)
+{
+
+  /* USER CODE BEGIN ADC_Init 0 */
+
+  /* USER CODE END ADC_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC_Init 1 */
+
+  /* USER CODE END ADC_Init 1 */
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc.Instance = ADC1;
+  hadc.Init.OversamplingMode = DISABLE;
+  hadc.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV1;
+  hadc.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc.Init.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  hadc.Init.ScanConvMode = ADC_SCAN_DIRECTION_FORWARD;
+  hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc.Init.ContinuousConvMode = DISABLE;
+  hadc.Init.DiscontinuousConvMode = DISABLE;
+  hadc.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc.Init.DMAContinuousRequests = DISABLE;
+  hadc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc.Init.Overrun = ADC_OVR_DATA_PRESERVED;
+  hadc.Init.LowPowerAutoWait = DISABLE;
+  hadc.Init.LowPowerFrequencyMode = ENABLE;
+  hadc.Init.LowPowerAutoPowerOff = DISABLE;
+  if (HAL_ADC_Init(&hadc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure for the selected ADC regular channel to be converted.
+  */
+  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Rank = ADC_RANK_CHANNEL_NUMBER;
+  if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC_Init 2 */
+
+  /* USER CODE END ADC_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LD2_Pin|GPIO_PIN_8|GPIO_PIN_10, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LD2_Pin PA8 PA10 */
+  GPIO_InitStruct.Pin = LD2_Pin|GPIO_PIN_8|GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PC7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ third_project/Debug/Drivers/STM32L0xx_HAL_Driver/stm32l0xx_hal_pwr_ex.d
@@ -0,0 +1,92 @@
+Drivers/STM32L0xx_HAL_Driver/stm32l0xx_hal_pwr_ex.o: \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_pwr_ex.c \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h \
+ ../Core/Inc/stm32l0xx_hal_conf.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l053xx.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/CMSIS/Include/core_cm0plus.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/CMSIS/Include/mpu_armv7.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_exti.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_adc.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_adc_ex.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart.h \
+ /home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart_ex.h
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h:
+
+../Core/Inc/stm32l0xx_hal_conf.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l053xx.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/CMSIS/Include/core_cm0plus.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/CMSIS/Include/cmsis_version.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/CMSIS/Include/mpu_armv7.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_exti.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_adc.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_adc_ex.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart.h:
+
+/home/kveriksen/STM32Cube/Repository/STM32Cube_FW_L0_V1.12.0/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_uart_ex.h:
